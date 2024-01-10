1178aa8623cbdc8935014527c2c52362fea385fd
perry-keanou nowak <pkn@MacBook-Pro-de-perry-keanou.local>
2024-01-10 12:41:03 +0100

    find problem whit return endingstrim

--- ft_strtrimstr.c
+++ ft_strtrimstr.c
@@ -16,6 +16,7 @@ static size_t ft_strlen_const(const char   *str){
 //fonction qui va parcour x char de s1 de la longueur de set, pour savoir si ils ont exactement les mêmes valeurs.
 //si il y a une différence avant la fin de la boucle de set, on retourn 0.
 //si tout est ok, on répète la boucle au cas ou il y aurait plusieurs set d'affilé dans s1. 
+//OK POUR LE START
 size_t ft_startingtrim(const char *paramS1,  const char *paramSet){
 	size_t rslt;//cette variable va renvoyer le nb de fois où l'on trouve set OU le nb de chars  à trim (qui se le nombre de boucle de while * set)
 	size_t i;
@@ -24,10 +25,9 @@ size_t ft_startingtrim(const char *paramS1,  const char *paramSet){
 	rslt = 0;
 	i = 0;
 	j = 0;
-	printf("start ------- paramS1[i] = %c  == paramSet[j] = %c\n", paramS1[i], paramSet[j]);
 	while(paramS1[i] != '\0' && paramS1[i] == paramSet[j]){
 		//si j egale la lonueur de set - 1 car on commence j à zero.
-		printf(" - - - - - i = %zu && rslt = %zu\n",i,rslt);
+		
 		if(paramS1[i] == paramSet[j] && j == ft_strlen_const(paramSet) - 1){
 			rslt += 1;
 			j = 0;
@@ -39,13 +39,11 @@ size_t ft_startingtrim(const char *paramS1,  const char *paramSet){
 		}
 		i++;
 	}
-	printf(" - - - - - i = %zu && rslt = %zu\n",i,rslt);
-	printf("start ------- paramS1[i] = %c  == paramSet[j] = %c\n", paramS1[i], paramSet[j]);// pourquoi paramS1[i] n'affiche rien ?
 	//printf("paramS1 --> %s\n", paramS1);
 	//printf("i --> %zu\n", i);
 	//printf("paramS1[i] --> %c\n", paramS1[i]);
 	//paramS1[i] == ' ';
-	return(rslt);
+	return(rslt * ft_strlen_const(paramSet));
 }
 
 
@@ -56,15 +54,17 @@ size_t ft_startingtrim(const char *paramS1,  const char *paramSet){
 */
 size_t ft_endingtrim(const char *paramS1, const char *paramSet){
 	size_t rslt;
-	size_t i;;
-	size_t end;
+	size_t i;
+	size_t j;
+	size_t len_s1;
 	size_t len_set;
 	int booleTrue;
 
 	rslt = 0;
 	i = 0;
+	j = 1;
 	booleTrue = 1;
-	end = ft_strlen_const(paramS1);//longueur total de la chaine, donc ex : 10 char.
+	len_s1= ft_strlen_const(paramS1);//longueur total de la chaine, donc ex : 10 char.
 	len_set = ft_strlen_const(paramSet);
 	//end = ft_strlen_const(paramS1 - 1);Ceci est la longueur - 1 donc pour préciser que l'on commence un index à 0.
 	//on doit boucler sur tout les chars de s1 en partant de la fin.
@@ -77,15 +77,17 @@ size_t ft_endingtrim(const char *paramS1, const char *paramSet){
 	while(booleTrue == 1){
 		printf("---------------------- 2 --------------------\n");
 		//len_set - 1 car on commence i à 0
-		if(paramS1[end - len_set - 1] == paramSet[i] && i == len_set - 1){
+		//ici dans l'index de paramS1, on doit avoir comme valeur, len_s1 - (len_set * end)
+		if(paramS1[len_s1 - (len_set * j)] == paramSet[i] && i == len_set - 1){
 			printf("----------------------3 --------------------\n");
 			rslt += 1;
 			i = 0;//ici je remets la set à l'index 0 
 			//je dois aussi décrémenter de len set la chaine s1 pour relancer la boucle
-			end -= len_set;
-		}else if(paramS1[end - len_set - 1] == paramSet[i] && i < len_set){
+			len_s1 -= len_set;
+			j++;
+		}else if(paramS1[len_s1 - len_set - 1] == paramSet[i] && i < len_set){
 			printf("---------------------- 4 --------------------\n");
-			end--;
+			len_s1--;
 		}else{
 			printf("---------------------- 5 --------------------\n");
 			booleTrue = 0;
@@ -95,7 +97,7 @@ size_t ft_endingtrim(const char *paramS1, const char *paramSet){
 		printf("---------------------- 6 --------------------\n");
 	}
 	printf("---------------------- 7 --------------------\n");
-	return(rslt);
+	return(rslt * ft_strlen_const(paramSet));
 }
 /*
 Cette fonction va supprimer le debut et fin de chaine de s1 si on retouve exactement set en debut et fin.
@@ -121,6 +123,7 @@ char    *ft_strtrimstr(const char *s1, const char    *set){
 	if(s1[i] != set[i])
 	{
 		printf("---- 2 -----\n");
+		//OK POUR LE START
 		start = 0;
 	}else{
 		printf("---- 3 -----\n");
@@ -128,13 +131,15 @@ char    *ft_strtrimstr(const char *s1, const char    *set){
 		//
 		//ici, il faudrait avoir un fn qui incrémente start tant qu'il retourne "true"
 		// il faudrait boucler directement dans ft_startingtrim
+		//OK POUR LE START
 		start = ft_startingtrim(s1,  set);
 		
 	}
 	printf("---- 4 -----\n");
-	if(s1[lens1 - ft_strlen_const(set) - 1] != set[i]){
+	if(s1[lens1 - ft_strlen_const(set)] != set[i]){
 		printf("---- 5 -----\n");
 		printf("s1[lens1 - ft_strlen_const(set) - 1] --> %c\n",s1[lens1 - ft_strlen_const(set) - 1]);
+		printf("set[%zu] --> %c\n", i, set[i]);
 		end = 0;
 	}else{
 		//cette fonction va retourner un size_t, qui selon la fn peut etre un de la taille de set ou de x * set 
@@ -143,15 +148,21 @@ char    *ft_strtrimstr(const char *s1, const char    *set){
 		printf("---- 6 -----\n");
 		end = ft_endingtrim(s1, set);
 	}
+	printf("start --> %zu\n", start);
+	printf("end --> %zu\n", end);
+	printf("lenS1 --> %zu\n", ft_strlen_const(s1));
+	printf("len_set --> %zu\n", ft_strlen_const(set));
+	printf("lenS1 - start - end + 1 ==> %zu\n", lens1 - start - end + 1);
 	printf("---- 7-----\n");
-	str = (char *)malloc(lens1 - start - end + 2 * sizeof(char));
+	str = (char *)malloc(lens1 - start - end + 1 * sizeof(char));
 	if(str == NULL){
 		return(NULL);
 	}
 	printf("---- 8 -----\n");
-	while(s1[i] != '\0' && i < lens1 - start - end){
+	while(s1[i] != '\0' && i < lens1 - start - end - 1){
 		printf("---- 9 -----\n");
 		str[i] = s1[start];
+		printf("s1[%zu] --> %c && s1[%zu] --> %c\n",i,s1[i], start ,s1[start]);
 		i++;
 		start++; 
 	}
